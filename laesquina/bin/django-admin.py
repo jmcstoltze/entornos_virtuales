04b1e37f4fa9b24ccb69fc0a561fde2e6c04b8e7
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-04-05 22:20:00 -0300

    Commit Inicial

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jmcstolz/proyectos_bootcamp_django/entornos_virtuales/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
